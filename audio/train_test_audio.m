bb722bda2bee3acb2ac65e072b64f09595ba8ca8
JingkaiYan <yan_jingkai@hotmail.com>
2017-01-01 17:32:44 +0800

    update audio model & add audio predict prob

--- audio/train_test_audio.m
+++ audio/train_test_audio.m
@@ -1,2 +1,34 @@
-model = svmtrain(labelAudio,featureAudio);
+model = svmtrain(labelAudio,featureAudio, '-t 1 -d 1');
 [predict_label, accuracy, dec_values] =svmpredict(labelAudioVal,featureAudioVal, model);
+
+prob = zeros(length(labelAudioVal),7);
+id = [1 2 3 4 5 6;
+      1 7 8 9 10 11;
+      2 7 12 13 14 15;
+      3 8 12 16 17 18;
+      4 9 13 16 19 20;
+      5 10 14 17 19 21;
+      6 11 15 18 20 21];
+weight = [1 1 1 1 1 1;
+          -1 1 1 1 1 1;
+          -1 -1 1 1 1 1;
+          -1 -1 -1 1 1 1;
+          -1 -1 -1 -1 1 1;
+          -1 -1 -1 -1 -1 1;
+          -1 -1 -1 -1 -1 -1];
+for i = 1:7
+    prob(:,i) = dec_values(:,id(i,:)) * weight(i,:)';
+end
+prob = exp(prob);
+for i = 1:length(labelAudioVal)
+    prob(i,:) = prob(i,:) / sum(prob(i,:));
+end
+
+predict_label_prob = zeros(length(labelAudioVal),1);
+for i = 1:length(labelAudioVal)
+    for j = 2:7
+        if prob(i,j)>prob(i,predict_label_prob(i)+1)
+            predict_label_prob(i) = j-1;
+        end
+    end
+end
